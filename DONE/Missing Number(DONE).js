734694f9afaa7774007311c9df1c2cdab48ec8ea
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-06-12 16:12:39 +0400

    Missing Number JS

---
+++ DONE/Missing Number(DONE).js
@@ -0,0 +1,11 @@
+var missingNumber = function (nums) {
+  let res = nums.length
+  for (let i = 0; i < nums.length; i++) {
+    res ^= i ^ nums[i]
+  }
+  return res
+}
+
+console.log(missingNumber([3, 0, 1])) //2
+console.log(missingNumber([0, 1])) //2
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])) //8
